bde12ecf1149fed9da24dc1a0796f8f713e020fd
unknown <Kurganov.2002@bk.ru>
2023-10-09 18:10:52 +0300

    first commit

---
+++ SoftwareDesignLab1/SoftwareDesignLab1/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿
+namespace SoftwareDesignLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.order_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.order_phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.order_destination = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.order_cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.order_driver_score = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.order_table_id = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.order_operator_id = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Selection = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.driver_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Procedure = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.order_id,
+            this.order_phone,
+            this.order_destination,
+            this.order_cost,
+            this.order_driver_score,
+            this.order_table_id,
+            this.order_operator_id});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(763, 349);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // order_id
+            // 
+            this.order_id.DataPropertyName = "order_id";
+            this.order_id.HeaderText = "id";
+            this.order_id.MinimumWidth = 6;
+            this.order_id.Name = "order_id";
+            this.order_id.ReadOnly = true;
+            this.order_id.Width = 125;
+            // 
+            // order_phone
+            // 
+            this.order_phone.DataPropertyName = "order_phone";
+            this.order_phone.HeaderText = "Телефон";
+            this.order_phone.MinimumWidth = 6;
+            this.order_phone.Name = "order_phone";
+            this.order_phone.Width = 125;
+            // 
+            // order_destination
+            // 
+            this.order_destination.DataPropertyName = "order_destination";
+            this.order_destination.HeaderText = "Пункт назначения";
+            this.order_destination.MinimumWidth = 6;
+            this.order_destination.Name = "order_destination";
+            this.order_destination.Width = 125;
+            // 
+            // order_cost
+            // 
+            this.order_cost.DataPropertyName = "order_cost";
+            this.order_cost.HeaderText = "Цена";
+            this.order_cost.MinimumWidth = 6;
+            this.order_cost.Name = "order_cost";
+            this.order_cost.Width = 125;
+            // 
+            // order_driver_score
+            // 
+            this.order_driver_score.DataPropertyName = "order_driver_score";
+            this.order_driver_score.HeaderText = "Оценка водителю";
+            this.order_driver_score.MinimumWidth = 6;
+            this.order_driver_score.Name = "order_driver_score";
+            this.order_driver_score.Width = 125;
+            // 
+            // order_table_id
+            // 
+            this.order_table_id.DataPropertyName = "order_table_id";
+            this.order_table_id.HeaderText = "Таблица";
+            this.order_table_id.MinimumWidth = 6;
+            this.order_table_id.Name = "order_table_id";
+            this.order_table_id.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.order_table_id.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.order_table_id.Width = 125;
+            // 
+            // order_operator_id
+            // 
+            this.order_operator_id.DataPropertyName = "order_operator_id";
+            this.order_operator_id.HeaderText = "Оператор";
+            this.order_operator_id.MinimumWidth = 6;
+            this.order_operator_id.Name = "order_operator_id";
+            this.order_operator_id.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.order_operator_id.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.order_operator_id.Width = 125;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(582, 361);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(127, 40);
+            this.DeleteButton.TabIndex = 3;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(46, 361);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(190, 40);
+            this.UpdateButton.TabIndex = 5;
+            this.UpdateButton.Text = "Сохранить изменения";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(5, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(783, 437);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.DeleteButton);
+            this.tabPage1.Controls.Add(this.UpdateButton);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(775, 408);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Таблица";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.Selection);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(775, 408);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Выборка";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(55, 88);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(545, 303);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // Selection
+            // 
+            this.Selection.Location = new System.Drawing.Point(503, 37);
+            this.Selection.Name = "Selection";
+            this.Selection.Size = new System.Drawing.Size(106, 26);
+            this.Selection.TabIndex = 2;
+            this.Selection.Text = "Найти";
+            this.Selection.UseVisualStyleBackColor = true;
+            this.Selection.Click += new System.EventHandler(this.Selection_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(355, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Водитель:";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Controls.Add(this.Procedure);
+            this.tabPage3.Controls.Add(this.numericUpDown2);
+            this.tabPage3.Controls.Add(this.numericUpDown1);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(775, 408);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Хранимая процедура";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.driver_name});
+            this.dataGridView3.Location = new System.Drawing.Point(21, 68);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(494, 315);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // driver_name
+            // 
+            this.driver_name.DataPropertyName = "driver_name";
+            this.driver_name.HeaderText = "Водитель";
+            this.driver_name.MinimumWidth = 6;
+            this.driver_name.Name = "driver_name";
+            this.driver_name.Width = 125;
+            // 
+            // Procedure
+            // 
+            this.Procedure.Location = new System.Drawing.Point(531, 12);
+            this.Procedure.Name = "Procedure";
+            this.Procedure.Size = new System.Drawing.Size(126, 35);
+            this.Procedure.TabIndex = 3;
+            this.Procedure.Text = "Найти";
+            this.Procedure.UseVisualStyleBackColor = true;
+            this.Procedure.Click += new System.EventHandler(this.Procedure_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(361, 19);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            2023,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2021,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            2021,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(193, 19);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Заказы за месяц:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button Selection;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Procedure;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn driver_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order_phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order_destination;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order_cost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn order_driver_score;
+        private System.Windows.Forms.DataGridViewComboBoxColumn order_table_id;
+        private System.Windows.Forms.DataGridViewComboBoxColumn order_operator_id;
+    }
+}
+
---
+++ SoftwareDesignLab1/SoftwareDesignLab1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SoftwareDesignLab1
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString;
+
+        private SqlDataAdapter orderAdapter;
+        private SqlDataAdapter tableAdapter;
+        private SqlDataAdapter operatorAdapter;
+
+        private SqlCommandBuilder orderBuilder = new SqlCommandBuilder();
+
+        private DataSet dataSet = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+
+            orderAdapter = new SqlDataAdapter("Select * from TaxiOrder", connectionString);
+            tableAdapter = new SqlDataAdapter("Select * from TaxiTable", connectionString);
+            operatorAdapter = new SqlDataAdapter("Select * from TaxiOperator", connectionString);
+
+            orderBuilder = new SqlCommandBuilder(orderAdapter);
+
+            orderAdapter.Fill(dataSet, "order");
+            tableAdapter.Fill(dataSet, "table");
+            operatorAdapter.Fill(dataSet, "operator");
+
+            dataGridView1.DataSource = dataSet.Tables["order"];
+
+            FillComboBox1();
+            FillTableCombobox();
+            FillOperatorCombobox();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            orderAdapter.Update(dataSet, "order");
+        }
+
+        private void FillTableCombobox()
+        {
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["order_table_id"]).DataSource =
+                dataSet.Tables["table"];
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["order_table_id"]).DisplayMember =
+                "table_date_start";
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["order_table_id"]).ValueMember =
+                "table_id";
+        }
+
+        private void FillOperatorCombobox()
+        {
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["order_operator_id"]).DataSource =
+                dataSet.Tables["operator"];
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["order_operator_id"]).DisplayMember =
+                "operator_name";
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["order_operator_id"]).ValueMember =
+                "operator_id";
+        }
+        private void FillComboBox1()
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("select * from TaxiDriver", sqlConnection);
+
+                DataSet dataSet = new DataSet();
+                sqlAdapter.Fill(dataSet, "car");
+
+                comboBox1.DataSource = dataSet.Tables["car"];
+                comboBox1.DisplayMember = "driver_name";
+                comboBox1.ValueMember = "driver_id";
+            }
+        }
+
+        private void Selection_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand =
+                    new SqlCommand("SELECT n.order_phone, n.order_cost" +
+                                  " FROM TaxiDriver p join TaxiTable m on p.driver_id = m.table_driver_id join TaxiOrder n on m.table_id = n.order_table_id" +
+                                  " where p.driver_id = " + comboBox1.SelectedValue
+                                  , sqlConnection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                DataTable dataTable = new DataTable("report1");
+                dataTable.Columns.Add("Телефон");
+                dataTable.Columns.Add("Цена");
+                while (sqlDataReader.Read())
+                {
+                    DataRow row = dataTable.NewRow();
+                    row["Телефон"] = sqlDataReader["order_phone"];
+                    row["Цена"] = sqlDataReader["order_cost"];
+                    dataTable.Rows.Add(row);
+                }
+
+                sqlDataReader.Close();
+                dataGridView2.DataSource = dataTable;
+            }
+        }
+
+        private void Procedure_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("order_byMonth", sqlConnection);
+                sqlAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlAdapter.SelectCommand.Parameters.Add(new SqlParameter("@month", SqlDbType.Int));
+                sqlAdapter.SelectCommand.Parameters["@month"].Value = numericUpDown1.Value;
+
+                sqlAdapter.SelectCommand.Parameters.Add(new SqlParameter("@year", SqlDbType.Int));
+                sqlAdapter.SelectCommand.Parameters["@year"].Value = numericUpDown2.Value;
+
+                DataSet dataSet = new DataSet();
+                sqlAdapter.Fill(dataSet, "procedure");
+
+                dataGridView3.DataSource = dataSet.Tables["procedure"];
+            }
+        }
+    }
+}
